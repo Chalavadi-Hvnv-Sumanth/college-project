e14790e1f14d40199dbffb07f746cfe25a1d8b7b
Chalavadi Hvnv Sumanth <csumanth81@gmail.com>
2026-01-26 20:33:18 +0530

    Remove Supabase client credentialsre
    Removed Supabase credentials for security reasons.

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 // Use the JS library to create a bucket.
 import { createClient } from '@supabase/supabase-js';
-const supabase= createClient('azzxcjxmbcpimyjgldgt',"sb_secret_wN58kJTLrvGlEIbC9QlC2Q_8sjrGWeU");
+const supabase= createClient('");
 // const { data, error } = await supabase.storage.createBucket('avatars', {// default: false
 // });
 export default supabase;
