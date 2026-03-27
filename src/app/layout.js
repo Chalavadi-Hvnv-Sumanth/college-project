a7f0bd551c565732a50474ae68e609d95afd3cd7
Mr-Sumanth <csumanth81@gmail.com>
2026-03-27 11:01:16 +0530

    some code was added

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "VVITU Repository",
-  description: "",
+  description: "The VVIT Repository website is a web-based platform designed to provide students with easy access to academic resources, project materials, and useful information related to their college studies. It acts as a centralized hub where users can explore, organize, and retrieve content efficiently.",
   icons: {
     icon: [
       { url: "https://www.vvitu.ac.in/src/assets/images/VVIT_logo.pn" }
@@ -29,7 +29,9 @@ export default function RootLayout({ children }) {
   href="/VVIT.png"
   type="image/<generated>"
   sizes="<generated>"
-/></head>
+/>
+<meta name="google-site-verification" content="q-fgCC4xlwkadgO_7dblSqDph8vmrBhT_sBuP6M0VmY" />
+</head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
